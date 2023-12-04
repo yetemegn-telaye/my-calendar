54231098e4fab74688460f9c85d8339611ab8129
yetemegn-telaye <lemma19yet@gmail.com>
2023-12-05 00:56:26 +0300

    Update taskcard to loop through the labels to render the labels of each task

--- src/components/Calendar/TaskCard.tsx
+++ src/components/Calendar/TaskCard.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import "../../styles/TaskCard.css";
 
-interface TaskCardProps {
-    id: number;
-    title: string;
-    labels: Label[];
-}
 
-interface Label {
-    id: number;
-    text: string;
-    color: string;
-}
-
-const TaskCard: React.FC<any>  = ({task,setTask,index, allTasks,date}) => {
+const TaskCard: React.FC<any>  = ({task,labels}) => {
+ 
     return(
         <div className="task-card">
             <div className="task-labels">
-                {task.labels.map((label: any) => (
-                    <span key={label.id} title={label.text} className="label-color-box" style={{backgroundColor: label.color}}></span>
+                {labels.map((label:any) => (
+                    <span key={label.id} 
+                    title={label.text} 
+                    className="label-color-box"
+                     style={{backgroundColor: label.color}}></span>
                 ))}
                 
             </div>
             <div className="task-title">{task.title}</div>
+            
         </div>
     )
 }
